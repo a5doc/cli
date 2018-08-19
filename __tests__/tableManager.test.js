b5079e9011a421f6604db0b72014181b6ca09ea4
msato <msato@altus5.com>
2018-08-19 17:43:15 +0900

    テストのデータセットをfixtureに作成

--- __tests__/tableManager.test.js
+++ __tests__/tableManager.test.js
@@ -1,17 +1,16 @@
 'use strict';
 
 const a5conf = require('../lib/conf')({
-  tableFinder: './__tests__/data/tableManager/**/*.yml'
+  table: {
+    src: './__tests__/fixture/simple-table/**/*.yml',
+    output: './.tmp/table-md'
+  }
 });
 const tableManager = require('../lib/tableManager');
 
 test('read table yml', () => {
-  expect(tableManager.all()).toEqual({
-    table1: {
-      id: 'table1'
-    },
-    table2: {
-      id: 'table2'
-    }
-  });
+  const actual = tableManager.readAll();
+  expect(Object.keys(actual).length).toBe(2);
+  expect(actual['table1'].id).toBe('table1');
+  expect(actual['table2'].id).toBe('table2');
 });
--- __tests__/util.test.js
+++ __tests__/util.test.js
@@ -3,10 +3,10 @@
 const util = require('../lib/util');
 
 test('findFilesGlob', () => {
-  const pattern = './__tests__/data/tableManager/**/*.yml';
+  const pattern = './__tests__/fixture/simple-table/**/*.yml';
   let actual = util.findFilesGlob(pattern);
-  expect(actual).toContain('./__tests__/data/tableManager/table1.yml');
-  expect(actual).toContain('./__tests__/data/tableManager/sub/table2.yml');
+  expect(actual).toContain('./__tests__/fixture/simple-table/table1.yml');
+  expect(actual).toContain('./__tests__/fixture/simple-table/sub/table2.yml');
   expect(actual.length).toBe(2);
   actual = util.findFilesGlob([pattern]);
   expect(actual.length).toBe(2);
